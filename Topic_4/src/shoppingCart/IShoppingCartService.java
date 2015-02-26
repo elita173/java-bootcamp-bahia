ee815d49b67f632a20b6c550b5dfb9fd5dc8debf
elita173 <eli_173_sole@yahoo.com.ar>
2015-02-26 12:13:54 -0300

    Topic 4 completed.

--- Topic_4/src/shoppingCart/IShoppingCartService.java
+++ Topic_4/src/shoppingCart/IShoppingCartService.java
@@ -1,10 +1,11 @@
 package shoppingCart;
 
 /**
- * Interface to a Shopping Cart Service It is assumed one cart per client: when
- * a new cart is created, it deletes the existent one and creates a new one.
- * When the client executes the payment, a new order is created and stored; and
- * a new cart is created.
+ * Interface to a Shopping Cart Service. It manages a set of shopping carts and
+ * a set of transactions made by clients. It is assumed one cart per client:
+ * when a new cart is created, it deletes the existent one and creates a new
+ * one. When the client executes the payment, a new order is created and stored;
+ * and a new cart is created.
  * 
  * @author Elisabet Arratia
  */
@@ -15,6 +16,8 @@ public interface IShoppingCartService {
 	 *
 	 * @param userId
 	 *            a String with the client's ID for which a new cart is created.
+	 *            If the user already has a shopping cart, it is replaced by the
+	 *            new one.
 	 */
 	public void createCart(String userId);
 
@@ -35,7 +38,8 @@ public interface IShoppingCartService {
 	 *            a String with the client's ID.
 	 * @param idProduct
 	 *            The ID of the product that is to be added to the cart of the
-	 *            user with ID <code>idUser</code>
+	 *            user with ID <code>idUser</code>. If the product already
+	 *            exists, it adds it again.
 	 * @param quantity
 	 *            The quantity of the product added to the cart
 	 */
@@ -48,7 +52,8 @@ public interface IShoppingCartService {
 	 *            a String with the client's ID.
 	 * @param idProduct
 	 *            the ID of the Product to be removed from the cart of the user
-	 *            with ID <code>idUser</code>
+	 *            with ID <code>idUser</code>. If the item doesn't exist, the
+	 *            method doesn't do anything.
 	 */
 	public void removeItem(String userId, int idProduct);
 
@@ -57,16 +62,23 @@ public interface IShoppingCartService {
 	 *
 	 * @param userId
 	 *            a String with the client's ID.
+	 * 
+	 * @return the shopping cart of user <code>idUser</code>. If the user
+	 *         doesn't exists, it returns null.
 	 */
 	public ShoppingCart getShoppingCart(String userId);
 
 	/**
 	 * Gets the number of Shopping carts in the DB
+	 * 
+	 * @return the number of Shopping carts in the DB
 	 */
 	public int numberOfShoppingCarts();
 
 	/**
 	 * Gets the number of transactions in the DB.
+	 * 
+	 * @return the number of transactions in the DB
 	 */
 	public int numberOfTransactions();
 }
